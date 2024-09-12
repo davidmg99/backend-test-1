0650ec5e15b1f51c7342c49f1ac5bd95379d4542
david.martinez <74658573+DVdivy@users.noreply.github.com>
2024-09-12 21:13:35 +0200

    First example POST

--- src/routes/invoice_routes.py
+++ src/routes/invoice_routes.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, request, jsonify
+from services import invoice_service
 
 invoice_blueprint = Blueprint('invoice_blueprint', __name__)
 
 @invoice_blueprint.route('/invoice', methods=['GET'])
 def get_invoices():
     return jsonify({'message':'aqui estan las facturas'})
+
+@invoice_blueprint.route('/invoice', methods=['POST'])
+def create_invoice():
+    body = request.get_json()
+    invoice = invoice_service.create_new_invoice(body)
+    return jsonify({'message' : invoice})
---
+++ src/services/invoice_service.py
@@ -0,0 +1,6 @@
+from models.invoice_model import Invoice
+from uuid import uuid4
+from datetime import datetime
+
+def create_new_invoice(body):
+    return "crear invoice"
