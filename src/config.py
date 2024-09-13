6ef93357e572affa78fafdbc08bcf8f30d2c293c
david.martinez <74658573+DVdivy@users.noreply.github.com>
2024-09-13 18:40:43 +0200

    Finished v1

--- src/config.py
+++ src/config.py
@@ -1,15 +1,12 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 
-# Inicializamos la instancia de la base de datos
 db = SQLAlchemy()
 
-# Clase base para configuraciones generales
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = '/app/invoices'
 
-# Configuración específica para desarrollo
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@db:5432/postgres'
