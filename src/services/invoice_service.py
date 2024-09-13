1c9ab04825862438966d82617f24ee5a80b8e570
david.martinez <74658573+DVdivy@users.noreply.github.com>
2024-09-13 16:06:23 +0200

    POST and GET working

--- src/services/invoice_service.py
+++ src/services/invoice_service.py
@@ -1,26 +1,32 @@
 import os
 from flask import current_app as app
 from werkzeug.utils import secure_filename
-from repositories import invoice_repository
-from models.invoice_model import Invoice, CategoryEnum
 from uuid import uuid4
 from datetime import datetime
+
+from repositories import invoice_repository
+from models.invoice_model import Invoice, CategoryEnum
+
 
 def create_new_invoice(body, file):
     if body['category'] not in [cat.value for cat in CategoryEnum]:
         raise ValueError("Invalid category")
 
+    if file and file.filename != "":
+        filename = secure_filename(file.filename)
+        file_extension = filename.split('.',)[1]
+        file_id = str(uuid4())
+        new_filename = file_id + "." + file_extension
 
-    filename = secure_filename(file.filename)
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(file_path)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], new_filename)
+        file.save(file_path)
 
     new_invoice = Invoice(
-        id=str(uuid4()),
+        id=file_id,
         category=body['category'],
         start_period=datetime.fromisoformat(body['startPeriod']),
         end_period=datetime.fromisoformat(body['endPeriod']),
-        file_url=f"http://localhost:8002/invoices/{uuid4()}.pdf"
+        file_url=f"http://localhost:8002/invoices/{file_id}.pdf"
     )
     invoice_repository.create_invoice(new_invoice)
     return new_invoice
